735765187959e0f1427f17e4419a36d4e16d6a92
kryptorado <doribra@gmail.com>
2019-06-06 13:31:51 -0400

    Added toolbar logo

---
+++ src/components/Navbar.vue
@@ -0,0 +1,28 @@
+<template>
+    <nav>
+        <v-toolbar dark>
+
+<!--            <v-toolbar-side-icon></v-toolbar-side-icon>-->
+            <v-toolbar-side-icon>
+                <img src="@/assets/watieat.png" style="width: 40px">
+            </v-toolbar-side-icon>
+            <v-toolbar-title>WatiEat</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-toolbar-items class="hidden-sm-and-down">
+                <v-btn flat>Link One</v-btn>
+                <v-btn flat>Link Two</v-btn>
+                <v-btn flat>Link Three</v-btn>
+            </v-toolbar-items>
+        </v-toolbar>
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: "Navbar"
+    }
+</script>
+
+<style scoped>
+
+</style>
